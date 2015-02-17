1ab6deb98f7dad52ac88f4510380b47396f7a40e
Don Denton <don@happycollision.com>
2015-02-17 02:12:42 -0600

    Beginnings

---
+++ app/controllers/index.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  header: "Hi, I'm Carol.",
+  image: "https://farm8.staticflickr.com/7397/15825596643_32f9c7173b_z.jpg",
+  header2: "I'd like to work with you.",
+  body: "I saw your ad in Backstage and would like to submit for the role of Rona Lisa Perretti in *The 25th Annual Putnam County Spelling Bee*.\n\nI've played the role twice before at [Rocky Mountain Repertory Theatre](http://www.rockymountainrep.com/) and Dominican University."
+});
