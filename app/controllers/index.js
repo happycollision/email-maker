7b40f963b75395ea099d0608e20b1260c1f417a5
Don Denton <don@happycollision.com>
2015-02-19 02:07:31 -0600

    Adds all initial values to controller for demo's sake

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,8 +3,13 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   header: "Hi, I'm Carol.",
   header2: "I'd like to work with you.",
-  body: "I saw your ad in Backstage and would like to submit for the role of Rona Lisa Perretti in *The 25th Annual Putnam County Spelling Bee*.\n\nI've played the role twice before at [Rocky Mountain Repertory Theatre](http://www.rockymountainrep.com/) and Dominican University.\n\n\n\nSincerely,  \nCarol Rose",
+  body: "I saw your ad in Backstage and would like to submit for the role of Rona Lisa Perretti in *The 25th Annual Putnam County Spelling Bee*.\n\nI've played the role twice before at [Rocky Mountain Repertory Theatre](http://www.rockymountainrep.com/) and Dominican University.\n\nSincerely,  \nCarol Rose",
 
   vButton: Em.computed.and('vButtonLink', 'vButtonText'),
-  rButton: Em.computed.and('rButtonLink', 'rButtonText')
+  rButton: Em.computed.and('rButtonLink', 'rButtonText'),
+
+  rButtonText: "View My Resumé",
+  rButtonLink: "http://happycollision.github.io/CarolRose-Website/resume/",
+  vButtonText: "Watch My Videos",
+  vButtonLink: "http://happycollision.github.io/CarolRose-Website/media/"
 });
