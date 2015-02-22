b9a430d060decb9bfaadafd5843cd01f8137c801
Don Denton <don@happycollision.com>
2015-02-21 18:32:45 -0600

    Info pop only show/hides on the trigger class element

--- app/views/info-pop.js
+++ app/views/info-pop.js
@@ -6,8 +6,9 @@ export default Ember.View.extend({
   classNames: ['info-helper'],
   active: false,
   click: function(event){
-    console.log(event);
     var active = this.get('active');
+    var triggerHit = event.target.className.indexOf('info-pop-trigger') > -1;
+    if (!triggerHit){return;}
     if (active) {
       this.set('active', false);
     } else {
