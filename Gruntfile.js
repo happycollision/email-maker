971f3cecb8da786d77f45e4e8f2cbb6856f11f9a
Don Denton <don@happycollision.com>
2015-02-23 12:48:45 -0600

    Adds Grunt-build-control
    Not fully ready yet, though.

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+var pkg = require('./package.json');
+
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Show elapsed time after tasks run
+  require('time-grunt')(grunt);
+  // Load all Grunt tasks
+  require('load-grunt-tasks')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+    // Task configuration.
+    exec:{
+      build:{
+        cmd: 'ember build --watch false --environment production'
+      }
+    },
+
+    buildcontrol:{
+      dist: {
+        options: {
+          dir: 'dist',
+          remote: 'git@github.com:happycollision/email-maker.git',
+          branch: 'gh-pages',
+          commit: true,
+          push: true,
+          tag: pkg.version
+        }
+      }
+    }
+
+  });
+  // Default task.
+  grunt.registerTask('default', ['build']);
+
+  grunt.registerTask('build', ['exec']);
+
+
+};
