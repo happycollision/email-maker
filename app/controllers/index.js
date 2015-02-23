4c59641f0f10e21d90d3e5a52dfacc58a2175da7
Don Denton <don@happycollision.com>
2015-02-23 01:50:09 -0600

    Adds widescreen styles for side-by-side

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   header: "Hi, I'm Carol.",
   header2: "I'd like to work with you.",
-  body: "I saw your ad in Backstage and would like to submit for the role of Rona Lisa Perretti in *The 25th Annual Putnam County Spelling Bee*.\n\nI've played the role twice before at [Rocky Mountain Repertory Theatre](http://www.rockymountainrep.com/) and Dominican University.\n\nSincerely,  \nCarol Rose\n\n[rosecarolrose@gmail.com](mailto:rosecarolrose@gmail.com)  \n[carolroseactress.com](http://www.carolroseactress.com)",
+  body: "I am excited to say things with *italics* and **bold** and [links](http://www.example.com)!\n\nSincerely,  \nCarol Rose\n\n[rosecarolrose@gmail.com](mailto:rosecarolrose@gmail.com)  \n[carolroseactress.com](http://www.carolroseactress.com)",
 
   vButton: Em.computed.and('vButtonLink', 'vButtonText'),
   rButton: Em.computed.and('rButtonLink', 'rButtonText'),
