78ee06b12c13fbbff25cbb34f7d51c19902d33b3
Don Denton <don@happycollision.com>
2015-02-24 16:15:51 -0600

    Moves index styles to main sheet
    This avoids the content flash. Users will be loading some unnecessary
    styles on their individual pages, but that is okay.

---
+++ app/controllers/application.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  dasherizedPath: function(){
+    return this.get('currentPath').replace(/\./g,'-');
+  }.property('currentPath')
+
+});
