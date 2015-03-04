153360afe94d6fd8da3f8ad54500c5af34a7d1aa
Don Denton <don@happycollision.com>
2015-03-04 04:12:44 -0600

    Add year to footer

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,6 +3,6 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   dasherizedPath: function(){
     return this.get('currentPath').replace(/\./g,'-');
-  }.property('currentPath')
-
+  }.property('currentPath'),
+  theYear: new Date().getFullYear()
 });
