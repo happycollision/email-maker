d6a2681af147eaa513fcf384489d3b7dc808c112
Don Denton <don@happycollision.com>
2015-03-06 00:54:27 -0600

    Removes call to no-longer-used-file

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,7 +4,6 @@ export default Ember.Route.extend({
   actions:{
     didTransition: function(){
       $('html head').find('.client-styles').remove();
-      $('html head').append('<link rel="stylesheet" class="client-styles" href="assets/client-styles/index.css">');
       FastClick.attach(document.body);
     }    
   }
